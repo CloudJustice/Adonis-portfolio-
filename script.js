6da44f7e2243077dcd3e01316c7b28dd2c41a88c
Adonis <136215708+CloudJustice@users.noreply.github.com>
2024-12-27 10:37:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-
-  document.addEventListener("DOMContentLoaded", () => {
-    emailjs.init("_I5ZuscZWtk7aGbMO"); // Replace with your EmailJS public key
-
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-
-      const name = form.name.value;
-      const email = form.email.value;
-      const message = form.message.value;
-
-      emailjs.send("service_fxks16o", "template_5nvm9hh", {
-        from_name: name,
-        from_email: email,
-        message: message,
-      })
-      .then(() => {
-        alert("Message sent successfully!");
-        form.reset();
-      })
-      .catch((error) => {
-        console.error("Failed to send message:", error);
-        alert("Failed to send the message. Please try again.");
-      });
-    });
-  });
+AOS.init();
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
